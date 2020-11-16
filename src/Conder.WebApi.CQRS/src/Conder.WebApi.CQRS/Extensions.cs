3d7b39c0f15d3b667c4d5a455967c515f566e212
Artem Matveychuk <pymba1996@live.com>
2020-11-16 18:24:43 +0500

    CQRS публичный доступ

---
+++ src/Conder.WebApi.CQRS/src/Conder.WebApi.CQRS/Extensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Conder.WebApi.CQRS.Builders;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Conder.WebApi.CQRS
+{
+    public static class Extensions
+    {
+        public static IApplicationBuilder UseDispatcherEndpoints(this IApplicationBuilder app,
+            Action<IDispatcherEndpointsBuilder> builder, bool useAuthorization = true,
+            Action<IApplicationBuilder> middleware = null)
+        {
+            var definitions = app.ApplicationServices.GetService<WebApiEndpointDefinitions>();
+
+            app.UseRouting();
+
+            if (useAuthorization)
+            {
+                app.UseAuthorization();
+            }
+
+            middleware?.Invoke(app);
+
+            app.UseEndpoints(router => builder(
+                new DispatcherEndpointsBuilder(
+                    new EndpointsBuilder(router, definitions))));
+
+            return app;
+        }
+    }
+}
