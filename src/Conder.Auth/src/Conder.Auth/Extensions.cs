e1b3fc9d3328b12b0cf1f7de6bb7a2a6082a25da
pymba86 <pymba-96@mail.ru>
2020-12-04 12:56:15 +0500

    jwt аутентификация

--- src/Conder.Auth/src/Conder.Auth/Extensions.cs
+++ src/Conder.Auth/src/Conder.Auth/Extensions.cs
@@ -1,7 +1,14 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
 using Conder.Auth.Handlers;
+using Conder.Auth.Middlewares;
+using Conder.Auth.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization.Policy;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Conder.Auth
 {
@@ -31,8 +38,144 @@ namespace Conder.Auth
             }
 
             builder.Services.AddSingleton<IJwtHandler, JwtHandler>();
+            builder.Services.AddSingleton<IAccessTokenService, MemoryAccessTokenService>();
+            builder.Services.AddTransient<AccessTokenValidatorMiddleware>();
+
+            if (options.AuthenticationDisabled)
+            {
+                builder.Services.AddSingleton<IPolicyEvaluator, DisabledAuthenticationPolicyEvaluator>();
+            }
+
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                RequireAudience = options.RequireAudience,
+                ValidateIssuer = options.ValidateIssuer,
+                ValidIssuers = options.ValidIssuers,
+                ValidateActor = options.ValidateActor,
+                ValidateAudience = options.ValidateAudience,
+                ValidAudiences = options.ValidAudiences,
+                ValidAudience = options.ValidAudience,
+                ValidIssuer = options.ValidIssuer,
+                ValidateLifetime = options.ValidateLifetime,
+                ValidateTokenReplay = options.ValidateTokenReplay,
+                ValidateIssuerSigningKey = options.ValidateIssuerSigningKey,
+                SaveSigninToken = options.SaveSigninToken,
+                RequireExpirationTime = options.RequireExpirationTime,
+                RequireSignedTokens = options.RequireSignedTokens,
+                ClockSkew = TimeSpan.Zero
+            };
+
+            if (!string.IsNullOrWhiteSpace(options.AuthenticationType))
+            {
+                tokenValidationParameters.AuthenticationType = options.AuthenticationType;
+            }
+
+            var hasCertificate = false;
+
+            if (options.Certificate is {})
+            {
+                X509Certificate2 certificate = null;
+
+                var password = options.Certificate.Password;
+                var hasPassword = !string.IsNullOrWhiteSpace(password);
+
+                if (!string.IsNullOrWhiteSpace(options.Certificate.Location))
+                {
+                    certificate = hasPassword
+                        ? new X509Certificate2(options.Certificate.Location, password)
+                        : new X509Certificate2(options.Certificate.Location);
+
+                    var keyType = certificate.HasPrivateKey ? "with private key" : "with public key";
+
+                    Console.WriteLine("Loaded X.509 certificate from location: " +
+                                      $"'{options.Certificate.Location}' {keyType}");
+                }
+
+                if (!string.IsNullOrWhiteSpace(options.Certificate.RawData))
+                {
+                    var rawData = Convert.FromBase64String(options.Certificate.RawData);
+
+                    certificate = hasPassword
+                        ? new X509Certificate2(rawData, password)
+                        : new X509Certificate2(rawData);
+
+                    var keyType = certificate.HasPrivateKey ? "with private key" : "with public key only";
+
+                    Console.WriteLine($"Loaded X.509 certificate from raw data: {keyType}");
+                }
+
+                if (certificate is {})
+                {
+                    if (string.IsNullOrWhiteSpace(options.Algorithm))
+                    {
+                        options.Algorithm = SecurityAlgorithms.RsaSha256;
+                    }
+
+                    hasCertificate = true;
+
+                    tokenValidationParameters.IssuerSigningKey = new X509SecurityKey(certificate);
+
+                    var actionType = certificate.HasPrivateKey ? "issuing" : "validating";
+
+                    Console.WriteLine($"Using X.509 certificate for {actionType} tokens");
+                }
+            }
+
+            if (!string.IsNullOrWhiteSpace(options.IssuerSigningKey) && !hasCertificate)
+            {
+                if (string.IsNullOrWhiteSpace(options.Algorithm) || hasCertificate)
+                {
+                    options.Algorithm = SecurityAlgorithms.HmacSha256;
+                }
+
+                var rawKey = Encoding.UTF8.GetBytes(options.IssuerSigningKey);
+                tokenValidationParameters.IssuerSigningKey = new SymmetricSecurityKey(rawKey);
+                Console.WriteLine("Using symmetric encryption for issuing tokens.");
+            }
+
+            if (!string.IsNullOrWhiteSpace(options.NameClaimType))
+            {
+                tokenValidationParameters.NameClaimType = options.NameClaimType;
+            }
+
+            if (!string.IsNullOrWhiteSpace(options.RoleClaimType))
+            {
+                tokenValidationParameters.RoleClaimType = options.RoleClaimType;
+            }
+
+            builder.Services
+                .AddAuthentication(o =>
+                {
+                    o.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    o.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer(o =>
+                {
+                    o.Authority = options.Authority;
+                    o.Audience = options.Audience;
+                    o.MetadataAddress = options.MetadataAddress;
+                    o.SaveToken = options.SaveToken;
+                    o.RefreshOnIssuerKeyNotFound = options.RefreshOnIssuerKeyNotFound;
+                    o.RequireHttpsMetadata = options.RequireHttpsMetadata;
+                    o.IncludeErrorDetails = options.IncludeErrorDetails;
+                    o.TokenValidationParameters = tokenValidationParameters;
+
+                    if (!string.IsNullOrWhiteSpace(options.Challenge))
+                    {
+                        o.Challenge = options.Challenge;
+                    }
+
+                    optionsFactory?.Invoke(o);
+                });
+
+            builder.Services.AddSingleton(options);
+
+            builder.Services.AddSingleton(tokenValidationParameters);
 
             return builder;
         }
+
+        public static IApplicationBuilder UseAccessTokenValidator(this IApplicationBuilder app)
+            => app.UseMiddleware<AccessTokenValidatorMiddleware>();
     }
 }
---
+++ src/Conder.Auth/src/Conder.Auth/JwtAuthAttribute.cs
@@ -0,0 +1,11 @@
+namespace Conder.Auth
+{
+    public class JwtAuthAttribute : AuthAttribute
+    {
+        public const string AuthenticationScheme = "Bearer";
+        
+        public JwtAuthAttribute(string policy = "") : base(AuthenticationScheme, policy)
+        {
+        }
+    }
+}
